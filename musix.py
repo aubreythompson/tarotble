56f40e85e715f6dfaeff63ba3fe04ba50cdcdc00
aubreyannthompson@gmail.com <aubreyannthompson@gmail.com>
2022-07-09 18:27:06 -0400

    calls to musixmatch

---
+++ musix.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+import ast
+app = Flask(__name__)
+api = Api(app)
+from musixmatch import Musixmatch
+
+APIKEY = '928dadc5c67821e645b1e38b6a414ddb'
+url = 'https://api.musixmatch.com/ws/1.1/'
+musixmatch = Musixmatch(APIKEY)
+musixmatch.matcher_lyrics_get(q_track='need your love', q_artist='Tennis')
+
+print("Success!")
