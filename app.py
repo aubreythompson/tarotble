c1dece6ce621623dc8199fef1bc834bb0e602b01
aubreyannthompson@gmail.com <aubreyannthompson@gmail.com>
2022-07-09 20:46:59 -0400

    gets lyrics

--- app.py
+++ app.py
@@ -1,27 +1,51 @@
-# from flask import Flask
-# from flask_restful import Resource, Api, reqparse
-# import pandas as pd
-# import ast
-# app = Flask(__name__)
-# api = Api(app)
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+import ast
+app = Flask(__name__)
+api = Api(app)
+from musixmatch import Musixmatch
+import json
+
+APIKEY = '928dadc5c67821e645b1e38b6a414ddb'
+url = 'https://api.musixmatch.com/ws/1.1/'
+musixmatch = Musixmatch(APIKEY)
+
 # from transformers import pipeline, set_seed
 
 # generator = pipeline('text-generation', model='gpt2')
 
 # set_seed(42)
 
-# gens = generator("""I need your love and I need your touch""", max_length=200, num_return_sequences=5)
 
-# for gen in gens:
-#     print('gens', gen)
 
-# print('length', len(gens))
 
-from musixmatch import Musixmatch
+@app.route('/lyrics',methods=['GET'])
+def lyrics():
+        
+    # parser = reqparse.RequestParser()  # initialize
+    # parser.add_argument('track', required=True)  # add arguments
+    # parser.add_argument('artist', required=True)
 
-APIKEY = '928dadc5c67821e645b1e38b6a414ddb'
-url = 'https://api.musixmatch.com/ws/1.1/'
-musixmatch = Musixmatch(APIKEY)
-lyrics = musixmatch.matcher_lyrics_get(q_track='need your love', q_artist='Tennis')
+    # args = parser.parse_args()  # parse arguments to dictionary
 
-print(lyrics)
+   
+    artist = request.args.get('artist')
+    track = request.args.get('track')
+    # track = args['track']
+    # artist = args['artist']
+
+    lyrics_dict = musixmatch.matcher_lyrics_get(q_track=track, q_artist=artist)
+  #  lyrics_dict = json.loads(lyrics_json)
+    if lyrics_dict['message']['header']['status_code']==200:
+        lyrics = lyrics_dict['message']['body']['lyrics']['lyrics_body']
+        # gens = generator("""I need your love and I need your touch""", max_length=50, num_return_sequences=1)
+        # print('length', len(gens))
+        return {'data': lyrics}, 200
+    else:
+        return {'message': "We couldn't find " + track + ' by ' + artist + ', sorry.'}, 401
+
+#api.add_resource(Lyrics,'/lyrics')
+
+if __name__ == '__main__':
+    app.run() 
